c27d33015d1e64288467fd7a0a0ef0ef7e3cee86
Clint Brodar <clintbrodar@Clints-MacBook-Pro.local>
2019-06-27 12:32:59 -0700

    Controller setup

--- models/burger.js
+++ models/burger.js
@@ -8,12 +8,12 @@ var burger = {
       });
     },
     // The variables cols and vals are arrays.
-    createOne: function(cols, vals, cb) {
+    create: function(cols, vals, cb) {
       orm.insertOne("burgers", cols, vals, function(res) {
         cb(res);
       });
     },
-    updateOne: function(objColVals, condition, cb) {
+    update: function(objColVals, condition, cb) {
       orm.updateOne("burgers", objColVals, condition, function(res) {
         cb(res);
       });
