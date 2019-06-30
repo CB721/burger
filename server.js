d7f3c06e4d7881c569030e899995355ce67aa404
Clint Brodar <clintbrodar@Clints-MacBook-Pro.local>
2019-06-30 11:20:48 -0700

    Requirements re-done

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,6 +13,11 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers");
+
+app.use(routes);
 
 app.listen(PORT, function () {
     console.log("Server listening on: http://localhost:" + PORT);
